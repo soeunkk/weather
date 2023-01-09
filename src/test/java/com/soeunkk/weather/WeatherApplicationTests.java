e0b8a05ae782543ce76b65061c29a53f70f9465e
soeunkk <luckyber1@naver.com>
2023-01-09 16:08:03 +0900

    Initial project

---
+++ src/test/java/com/soeunkk/weather/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soeunkk.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
